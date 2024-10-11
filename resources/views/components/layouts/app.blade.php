3061e0229e37f5409ed55ffa5a5f57746b73f9b9
Nishant Kedare <kedarenishant11111@gmail.com>
2024-10-11 16:08:48 +0530

    Vite issue fixed

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    @vite('resources/css/app.css','resources/js/app.js')
+    @vite('resources/css/app.css')
     <title>{{ 'Patrisen' }}</title>
     <link rel="icon" class="rounded-full" type="image/x-icon" href="{{ asset('images/Patrisen_favicon.png') }}">
     <script src="{{asset('js/events.js')}}"></script>
